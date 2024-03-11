f75fd1fb06becb012a697135fcee954badbbcc2f
yungun12 <yungun200512@gmail.com>
2024-03-11 15:14:12 +0900

    hello

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello!! My First Java Coding!");
 
+        System.out.println("Edit java Code!!");
     }
 }
