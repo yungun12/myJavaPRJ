b13fcb3a00a00342bc3976c81b3cd3e4a397c5ae
yungun12 <yungun200512@gmail.com>
2024-03-11 15:16:47 +0900

    hello

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
         System.out.println("Hello!! My First Java Coding!");
 
         System.out.println("Edit java Code!!");
+
+        System.out.println("깃허브와 친해지기!!");
     }
 }
