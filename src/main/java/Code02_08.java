277630404d2befa342d904ea7022a1e20aa92fd4
yungun12 <yungun200512@gmail.com>
2024-03-18 14:21:16 +0900

    hello

---
+++ src/main/java/Code02_08.java
@@ -0,0 +1,13 @@
+public class Code02_08 {
+    public static void main(String[] args) {
+        String str1 = "난생처음";
+        String str2 = "자바";
+
+        System.out.print(str1);
+        System.out.println(str2);
+
+        String result = str1 +str2;
+
+        System.out.println(result);
+    }
+}
