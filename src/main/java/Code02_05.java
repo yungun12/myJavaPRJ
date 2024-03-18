d02e00b6fce05fd195e0d963df186192c054e778
yungun12 <yungun200512@gmail.com>
2024-03-18 13:41:13 +0900

    hello

---
+++ src/main/java/Code02_05.java
@@ -0,0 +1,10 @@
+public class Code02_05 {
+    public static void main(String[] args) {
+
+        int a = 100;
+        int b = 200;
+
+        int c = a + b;
+        System.out.println(a + "+" + b + "=" + c);
+    }
+}
