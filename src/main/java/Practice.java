02a64391d1139320c70d8a8f24f88910cbceaede
yungun12 <yungun200512@gmail.com>
2024-03-19 11:31:34 +0900

    hello

---
+++ src/main/java/Practice.java
@@ -0,0 +1,19 @@
+public class Practice {
+    public static void main(String[] args) {
+        int num1, num2, result;
+        num1 = 1050;
+        num2 = 168;
+
+        result = num1 + num2;
+        System.out.println(num1 + "+" + num2 + "=" + result);
+
+        result = num1 - num2;
+        System.out.println(num1 + "-" + num2 + "-" + result);
+
+        result = num1 * num2;
+        System.out.println(num1 + "x" + num2 + "=" + result);
+
+        result = num1 / num2;
+        System.out.println(num1 + "/" + num2 + "=" + result);
+    }
+}
