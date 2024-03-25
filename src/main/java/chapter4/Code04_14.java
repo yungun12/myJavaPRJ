f5b8ecb620d76cacd3d68eab96374f1507186a3d
yungun12 <yungun200512@gmail.com>
2024-03-25 18:52:20 +0900

    hello

---
+++ src/main/java/chapter4/Code04_14.java
@@ -0,0 +1,9 @@
+package chapter4;
+public class Code04_14 {
+    public static void main(String[] args) {
+        String str = "난생처음 자바";
+
+        System.out.println(str.indexOf("난생"));
+        System.out.println(str.indexOf("처"));
+    }
+}
