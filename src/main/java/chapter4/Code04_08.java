db8b6cdd9281c7ba154f152bb116cc37b75cc5f4
yungun12 <yungun200512@gmail.com>
2024-03-25 18:42:13 +0900

    hello

---
+++ src/main/java/chapter4/Code04_08.java
@@ -0,0 +1,9 @@
+package chapter4;
+public class Code04_08 {
+    public static void main(String[] args) {
+        String var = "난생처음 \n" +
+                "자바를 \n" +
+                "열공 중입니다.";
+        System.out.println(var);
+    }
+}
