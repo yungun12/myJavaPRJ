663c03676dc96ad7b291bb929a2a2964a8ee92c5
yungun12 <yungun200512@gmail.com>
2024-03-29 12:52:34 +0900

    hello

---
+++ src/main/java/chapter3/Code03_02.java
@@ -0,0 +1,18 @@
+package chapter3;
+public class Code03_02 {
+    public static void main(String[] args) {
+        int n1, n2, res;
+        n1 = 5;
+        n2 = 3;
+        res = n1 + n2;
+        System.out.println(res);
+        res = n1 - n2;
+        System.out.println(res);
+        res = n1 * n2;
+        System.out.println(res);
+        res = n1 / n2;
+        System.out.println(res);
+        res = n1 % n2;
+        System.out.println(res);
+    }
+}
