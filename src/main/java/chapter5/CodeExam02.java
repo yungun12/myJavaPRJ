8803b9028e45cd5d94f0ee39d713d2881f6c6be2
yungun12 <yungun200512@gmail.com>
2024-04-01 18:29:56 +0900

    hello

---
+++ src/main/java/chapter5/CodeExam02.java
@@ -0,0 +1,27 @@
+package chapter5;
+import java.util.Scanner;
+public class CodeExam02 {
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        int num;
+
+        System.out.println("자연수를 입력하시오.");
+        num = s.nextInt();
+
+        if(num%15 == 0) {
+            System.out.println("3과 5의 배수입니다.");
+        }
+        else if (num%5 == 0) {
+            System.out.println("5의 배수입니다.");
+        }
+        else if (num%3 == 0) {
+            System.out.println("3의 배수입니다.");
+        }
+        else {
+            System.out.println("잘못된 선택입니다.");
+        }
+
+    s.close();
+    }
+}
+
