6f027718572a49b8ce7ee61661cce47caa73e4b8
yungun12 <yungun200512@gmail.com>
2024-04-02 11:55:11 +0900

    hello

---
+++ src/main/java/chapter3/Code03_01.java
@@ -0,0 +1,10 @@
+package chapter3;
+public class Code03_01 {
+    public static void main(String[] args) {
+        int n1,n2, res;
+        n1 = 5;
+        n2 = 3;
+        res = n1 + n2;
+        System.out.println(res);
+    }
+}
---
+++ src/main/java/chapter4/Code04_16.java
@@ -0,0 +1,12 @@
+package chapter4;
+public class Code04_16 {
+    public static void main(String[] args) {
+        String str = "Java";
+
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(1));
+        System.out.println(str.charAt(2));
+        System.out.println(str.charAt(3));
+        System.out.println(str.charAt(4));
+    }
+}
