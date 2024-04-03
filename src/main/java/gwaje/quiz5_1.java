c7e76320836ca43023bf28d3c7a3d7c2602864f0
yungun12 <yungun200512@gmail.com>
2024-04-03 19:41:06 +0900

    hello

---
+++ src/main/java/gwaje/quiz5_1.java
@@ -0,0 +1,26 @@
+package gwaje;
+import java.util.Scanner;
+public class quiz5_1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num;
+
+        System.out.println("자연수를 입력하세요:");
+        num = s.nextInt();
+
+        if(num%15 == 0) {
+            System.out.println("3과 5의 배수입니다.");
+        }
+        else if(num%5 == 0){
+            System.out.println("5의 배수입니다.");
+        }
+        else if (num%3 == 0){
+            System.out.println("3의 배수 입니다");
+        }
+        else {
+            System.out.println("잘못된 선택입니다.");
+        }
+
+        s.close();
+    }
+}
