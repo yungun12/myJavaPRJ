0011f04004360468582d701759b87f5633ec53f9
yungun12 <yungun200512@gmail.com>
2024-04-09 10:58:09 +0900

    hello

---
+++ src/main/java/green/Lab05_02.java
@@ -0,0 +1,45 @@
+package green;
+import java.util.Scanner;
+public class Lab05_02 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String myHand, comHand;
+        System.out.print("나의 가위/바위/보 ==> ");
+        myHand = s.next();
+
+        String[] hands = {"가위", "바위", "보"};
+        int rnd = (int) (Math.random() * hands.length);
+        comHand = hands[rnd];
+        System.out.println("컴퓨터의 가위/바위/보 ==> " + comHand);
+
+        if (myHand.equals("가위")) {
+            if (comHand.equals("가위"))
+                System.out.println("비겼습니다. ㅡ.ㅡ");
+            else if (comHand.equals("바위"))
+                System.out.println("졌습니다. ㅠㅠ");
+            else if (comHand.equals("보"))
+                System.out.println("이겼습니다. ^^");
+        }
+        else if (myHand.equals("바위")) {
+            if (comHand.equals("가위"))
+                System.out.println("이겼습니다. ^^");
+            else if (comHand.equals("바위"))
+                System.out.println(("비겼습니다 ㅡ.ㅡ"));
+            else if (comHand.equals("보"))
+                System.out.println("졌습니다. ㅠㅠ");
+        }
+        else if (myHand.equals("보")) {
+            if (comHand.equals("가위"))
+                System.out.println("졌습니다. ㅠㅠ");
+            else if (comHand.equals("바위"))
+                System.out.println("이겼습니다 ^^");
+            else if (comHand.equals("보"))
+                System.out.println("비겼습니다. ㅡ.ㅡ");
+        }
+        else {
+            System.out.println("가위/바위/보 중 하나를 내새요.");
+        }
+
+        s.close();
+    }
+}
