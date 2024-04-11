563bf90830a67862524f3bc0291a390aea1ef82a
yungun12 <yungun200512@gmail.com>
2024-04-11 18:06:43 +0900

    hello

---
+++ src/main/java/C0c0123.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class C0c0123{
+    public static void main(String[]args){
+
+        Scanner s = new Scanner(System.in);
+
+        int a,b,c;
+        a= s.nextInt();
+        b=s.nextInt();
+        c = a * b;
+
+        System.out.println(c);
+        s.close();
+    }
+}
+
