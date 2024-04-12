a328d857305535a6e26d186ae5c230cb97510489
yungun12 <yungun200512@gmail.com>
2024-04-12 19:43:56 +0900

    hello

---
+++ src/main/java/Memo/Memo01.java
@@ -0,0 +1,18 @@
+package Memo;
+import java.util.Scanner;
+public class Memo01 {
+        public static void main(String[]args) {
+            Scanner s = new Scanner(System.in);
+            int a,b;
+
+            System.out.print("");
+            a = s.nextInt();
+            System.out.print("");
+            b = s.nextInt();
+
+
+            System.out.println(a * b);
+
+            s.close();
+        }
+}
