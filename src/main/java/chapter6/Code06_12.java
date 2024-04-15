00e7e23fd1bd1231bef57b94e971e8498115bfe2
yungun12 <yungun200512@gmail.com>
2024-04-15 20:49:26 +0900

    hello

---
+++ src/main/java/chapter6/Code06_12.java
@@ -0,0 +1,21 @@
+package chapter6;
+
+import java.util.Scanner;
+
+public class Code06_12 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int hap = 0;
+        int num1, num2;
+
+        while(true) {
+            System.out.print("숫자 ==> ");
+            num1 = s.nextInt();
+            System.out.print("숫자2 ==> ");
+            num2 = s.nextInt();
+
+            hap = num1 + num2;
+            System.out.println(num1 + "+" + num2 + "=" + hap);
+        }
+    }
+}
