efe861c42477a6d5d6aac234c3352f0966118b80
yungun12 <yungun200512@gmail.com>
2024-04-24 20:59:39 +0900

    hello

---
+++ src/main/java/chapter6/Code06_05.java
@@ -0,0 +1,9 @@
+package chapter6;
+
+public class Code06_05 {
+    public static void main(String[] args) {
+        int hap;
+        hap = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+        System.out.println("1부터 10까지의 합계 : " + hap);
+    }
+}
