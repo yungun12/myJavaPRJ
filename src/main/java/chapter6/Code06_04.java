d660ad98fbe60306c8470020d14a07c0d9b8e784
yungun12 <yungun200512@gmail.com>
2024-05-02 08:29:33 +0900

    hello

---
+++ src/main/java/chapter6/Code06_04.java
@@ -0,0 +1,9 @@
+package chapter6;
+
+public class Code06_04 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
