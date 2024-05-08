892ae482deedc56544fc0c5a3ac192cd2e740a7c
yungun12 <yungun200512@gmail.com>
2024-05-08 17:07:26 +0900

    hello

---
+++ src/main/java/chapter7/Code07_06.java
@@ -0,0 +1,18 @@
+package chapter7;
+    class Rabbit0 {
+        String shape;
+        int xPos;
+        int yPos;
+
+        void setPosition(int x, int y) {
+            xPos = x;
+            yPos = y;
+        }
+    }
+
+public class Code07_06 {
+        public static void main(String[] args) {
+
+
+        }
+}
