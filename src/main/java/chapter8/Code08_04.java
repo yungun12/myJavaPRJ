9db04e75b742d6d16924f382a398824d2ca93ac1
yungun12 <yungun200512@gmail.com>
2024-05-16 16:09:33 +0900

    hello

---
+++ src/main/java/chapter8/Code08_04.java
@@ -0,0 +1,19 @@
+package chapter8;
+
+    class Rabbit8 {
+        Rabbit8 () {
+            System.out.println("1. 슈퍼 클래스(토끼)의 생성자가 호줄됩니다.");
+        }
+    }
+
+    class HouseRabbit extends Rabbit8 {
+        HouseRabbit () {
+            System.out.println("2. 서브 클래스(집토끼)의 생성자가 호줄됩니다.");
+        }
+    }
+
+public class Code08_04 {
+        public static void main(String[] args) {
+            HouseRabbit hRabbit = new HouseRabbit();
+        }
+}
