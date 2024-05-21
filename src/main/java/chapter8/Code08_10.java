05d1b8ecb81d0a1ca04e22fa5fdb6b84c043574f
yungun12 <yungun200512@gmail.com>
2024-05-21 15:32:27 +0900

    hello

---
+++ src/main/java/chapter8/Code08_10.java
@@ -0,0 +1,25 @@
+package chapter8;
+interface Car {
+    void move();
+}
+
+interface Cannon {
+    void fire();
+}
+
+class Tank implements Car, Cannon {
+    public void move() {
+        System.out.println("탱크가 앞으로 이동합니다.");
+    }
+    public void fire() {
+        System.out.println("탱크에서 대포를 발사합니다.");
+    }
+}
+
+public class Code08_10 {
+    public static void main(String[] args) {
+        Tank tank1 = new Tank();
+        tank1.move();
+        tank1.fire();
+    }
+}
