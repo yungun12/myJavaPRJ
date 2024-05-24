c79d58fddeac7bb10f1842cf35010db6d18d6d2b
yungun12 <yungun200512@gmail.com>
2024-05-24 09:49:21 +0900

    hello

--- src/main/java/chapter7/Code07_10.java
+++ src/main/java/chapter7/Code07_10.java
@@ -1,24 +1,23 @@
 package chapter7;
-    class Rabbit {
-        String shape;   //토끼의 모양
-        int xPos;       //x 위치
-        int yPos;       // y 위치
+class Rabbit5 {
+    String shape;
+    int xPos;
+    int yPos;
 
-        Rabbit() {
-            shape = "토끼";
-        }
-
-        void setPosition(int x, int y) {
-            xPos = x;
-            yPos = y;
-        }
+    Rabbit5() {
+        shape = "토끼";
     }
 
+    void setPosition(int x, int y) {
+        xPos = x;
+        yPos = y;
+    }
+}
 
-    public class Code07_10 {
-        public static void main(String[] args) {
-            Rabbit rabbit = new Rabbit();
+public class Code07_10 {
+    public static void main(String[] args) {
+        Rabbit5 rabbit = new Rabbit5();
 
-            System.out.printf("토끼 객체의 기본 모양은 [%s]입니다.", rabbit.shape);
-        }
+        System.out.printf("토끼 객체의 기본 모양은 [%s]입니다.", rabbit.shape);
+    }
 }
