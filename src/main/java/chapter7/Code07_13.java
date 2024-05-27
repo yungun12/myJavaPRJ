1b91d680f3c4caec8782f6c139fa098afe95d614
yungun12 <yungun200512@gmail.com>
2024-05-28 08:50:30 +0900

    hello

---
+++ src/main/java/chapter7/Code07_13.java
@@ -0,0 +1,15 @@
+package chapter7;
+class Rabbit7 {
+    public String shape;
+    public int xPos;
+    public int yPos;
+}
+
+public class Code07_13 {
+    public static void main(String[] args) {
+        Rabbit7 rabbit = new Rabbit7();
+
+        rabbit.xPos = 100;
+        rabbit.yPos = 200;
+    }
+}
