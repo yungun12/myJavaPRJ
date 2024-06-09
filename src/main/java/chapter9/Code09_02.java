7f7ac329d4f454bb5cceff30de283414d8ce1daa
yungun12 <yungun200512@gmail.com>
2024-06-10 08:35:23 +0900

    hello

---
+++ src/main/java/chapter9/Code09_02.java
@@ -0,0 +1,29 @@
+package chapter9;
+
+import java.util.Scanner;
+
+public class Code09_02 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[] numAry = new int[4];
+        int hap;
+
+        System.out.print("숫자 : ");
+        numAry[0] = s.nextInt();
+
+        System.out.print("숫자 : ");
+        numAry[1] = s.nextInt();
+
+        System.out.print("숫자 : ");
+        numAry[2] = s.nextInt();
+
+        System.out.print("숫자 : ");
+        numAry[3] = s.nextInt();
+
+        hap = numAry[0] + numAry[1] + numAry[2] + numAry[3];
+
+        System.out.println("합계 ==> " + hap);
+
+        s.close();
+    }
+}
