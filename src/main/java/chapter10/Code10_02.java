11a8e4eed12d4389e9b88ee736480bb0679db550
yungun12 <yungun200512@gmail.com>
2024-06-27 13:36:34 +0900

    hello

--- src/main/java/chapter10/Code10_02.java
+++ src/main/java/chapter10/Code10_02.java
@@ -1,4 +1,20 @@
 package chapter10;
 
 public class Code10_02 {
+    static void drawDice() {
+        int diceNum;
+        diceNum = (int) (Math.random() * 6);
+        System.out.println("결과 : " + diceNum);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("A님, 주사위를 던집니다~");
+        drawDice();
+
+        System.out.println("B님, 주사위를 던집니다~");
+        drawDice();
+
+        System.out.println("C님, 주사위를 던집니다~");
+        drawDice();
+    }
 }
