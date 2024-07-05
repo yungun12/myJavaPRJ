bb37748c27209552b8c0c65dcb057e0f48ebaf95
yungun12 <yungun200512@gmail.com>
2024-07-05 14:02:41 +0900

    hello

---
+++ src/main/java/chapter10/Code10_07.java
@@ -0,0 +1,24 @@
+package chapter10;
+
+public class Code10_07 {
+
+
+    public static int hap_para(int v1, int v2) {
+        int result = v1 + v2;
+        return result;
+
+    }
+    public static int hap_para(int v1, int v2, int v3) {
+        return hap_para(v1, v2) + v3;
+
+    }
+
+    public static void main(String[] args) {
+        int hap;
+        hap = hap_para(10, 20);
+        System.out.println("매개변수 2개 메서드 호줄 결과 ==> " + hap);
+
+        hap = hap_para(10, 20, 30);
+        System.out.println("매개변수 3개 메서드 호줄 결과 ==> " + hap);
+    }
+}
