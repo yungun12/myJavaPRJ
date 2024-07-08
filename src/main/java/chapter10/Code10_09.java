476f7a5d7b8becbc5378e4d22f5844c830a4a1d2
yungun12 <yungun200512@gmail.com>
2024-07-08 14:50:44 +0900

    hello

--- src/main/java/chapter10/Code10_09.java
+++ src/main/java/chapter10/Code10_09.java
@@ -1,4 +1,15 @@
 package chapter10;
 
 public class Code10_09 {
+    static int method2() {
+        int result;
+        result = 100;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int res;
+        res = method2();
+        System.out.println("method2()에서 돌려준 값 ==> " + res);
+    }
 }
