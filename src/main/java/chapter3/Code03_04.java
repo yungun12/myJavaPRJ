d4c4eba56c85c83d4d8b04f5b6c46fc957e58955
yungun12 <yungun200512@gmail.com>
2024-07-19 17:17:09 +0900

    hello

---
+++ src/main/java/chapter3/Code03_04.java
@@ -0,0 +1,10 @@
+package chapter3;
+
+public class Code03_04 {
+    public static void main(String[] args) {
+        double a = 2, b = 4, c = 6;
+        System.out.println(a / b * c);
+        System.out.println(a * c / b);
+        System.out.println(c / b * a);
+    }
+}
