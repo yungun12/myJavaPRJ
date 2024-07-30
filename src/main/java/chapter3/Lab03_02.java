7f491541d001a7f51f6f156bedbce47542151f54
yungun12 <yungun200512@gmail.com>
2024-07-30 10:59:41 +0900

    편의점 일일매출 계산하기

---
+++ src/main/java/chapter3/Lab03_02.java
@@ -0,0 +1,17 @@
+package chapter3;
+
+public class Lab03_02 {
+    public static void main(String[] args) {
+        int total = 0;
+
+        total -= 900*10;
+        total += 1800*2;
+        total -= 3500*5;
+        total += 4000*4;
+        total += 1500;
+        total += 2000*4;
+        total += 1800*5;
+
+        System.out.println("오늘 총매출액은 " + total + "원입니다.");
+    }
+}
