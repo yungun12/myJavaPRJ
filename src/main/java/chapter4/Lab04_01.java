c6970181e14da2886af608d29347a0c313aad24c
yungun12 <yungun200512@gmail.com>
2024-08-02 08:51:16 +0900

    문자열 거꾸로 출력하기

---
+++ src/main/java/chapter4/Lab04_01.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+public class Lab04_01 {
+    public static void main(String[] args) {
+        String ss = "블랙핑크";
+        System.out.println("원본 문자열 ==> " + ss);
+
+        System.out.print("반대 문자열 ==> ");
+        System.out.print(ss.charAt(3));
+        System.out.print(ss.charAt(2));
+        System.out.print(ss.charAt(1));
+        System.out.print(ss.charAt(0));
+    }
+}
