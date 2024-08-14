0513b632d383548f115a339ef427a22795f9c182
yungun12 <yungun200512@gmail.com>
2024-08-14 17:15:43 +0900

    같은 숫자가 나올 때까지 주사위 던지기

---
+++ src/main/java/chapter6/Lab06_03.java
@@ -0,0 +1,20 @@
+package chapter6;
+
+public class Lab06_03 {
+    public static void main(String[] args) {
+        int count = 0;
+        int dice1, dice2, dice3;
+
+        while(true) {
+            count++;
+            dice1 = (int)(Math.random()*6 + 1);
+            dice2 = (int)(Math.random()*6 + 1);
+            dice3 = (int)(Math.random()*6 + 1);
+
+            if((dice1 == dice2) && (dice2 == dice3))
+                break;
+        }
+        System.out.printf("3개 주사위는 모두 %d입니다.\n", dice1);
+        System.out.printf("같은 숫자가 나올 때까지 %d번 던졌습니다.\n", count);
+    }
+}
