b9eeb0057c25541c48d76d107281ccf75330cb8e
yungun12 <yungun200512@gmail.com>
2024-08-23 15:18:42 +0900

    배열의 요소 개수

---
+++ src/main/java/chapter9/Code09_05.java
@@ -0,0 +1,14 @@
+package chapter9;
+
+public class Code09_05 {
+    public static void main(String[] args) {
+        int ary[] = {10, 20, 30, 40, 50};
+        int count, size;
+
+        count = ary.length;
+        size = count * Integer.BYTES;
+
+        System.out.println("배열 ary[]의 요소 개수 : " + count);
+        System.out.println("배열 ary[]의 요소 전체 크기 : " + size);
+    }
+}
