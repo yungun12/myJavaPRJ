7d08038805b7c188aff55c16edebbce0b3d5921e
yungun12 <yungun200512@gmail.com>
2024-08-27 14:02:43 +0900

    for문의 다른 용법

---
+++ src/main/java/chapter9/Code09_07.java
@@ -0,0 +1,10 @@
+package chapter9;
+
+public class Code09_07 {
+    public static void main(String[] args) {
+        int[] ary = {10, 20, 30};
+        for (int data : ary) {
+            System.out.println(data);
+        }
+    }
+}
