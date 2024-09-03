aab073b4da0ae7a0de0af964780035a82f4317b3
yungun12 <yungun200512@gmail.com>
2024-09-03 14:29:09 +0900

    변수값 출력

---
+++ src/main/java/Chapter_2/Code02_04.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class Code02_04 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+
+        int c = a + b;
+        System.out.println(c);
+    }
+}
