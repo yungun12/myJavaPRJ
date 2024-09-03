d47b7677a1a122cc38c1c88da0de5f6fdc7d05f9
yungun12 <162670150+yungun12@users.noreply.github.com>
2024-09-03 14:25:50 +0900

    Delete src/main/java/chapter2 directory

--- src/main/java/chapter2/Code02_01.java
+++
@@ -1,7 +0,0 @@
-package chapter2;
-
-public class Code02_01 {
-    public static void main(String[] args) {
-        System.out.println("100 + 200");
-    }
-}
--- src/main/java/chapter2/Code02_02.java
+++
@@ -1,7 +0,0 @@
-package chapter2;
-    public class Code02_02{
-        public static void main(String[] args) {
-            int a = 100;
-            int b = 200;
-    }
-}
