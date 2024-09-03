6d24ae5281663c6e3aeb3c11c045455fdabdaf6d
yungun12 <yungun200512@gmail.com>
2024-09-03 14:59:46 +0900

    변수

--- src/main/java/chapter2/Code02_01.java
+++ src/main/java/chapter2/Code02_01.java
@@ -1,8 +1,9 @@
 package chapter2;
 
 public class Code02_01 {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        System.out.println("100 + 200");
-    }
+            System.out.println("100 + 200");
+        }
 }
+
