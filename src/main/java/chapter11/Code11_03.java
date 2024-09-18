bb99daa0aacf02a266a72e8abeea210b08e0b4c4
yungun12 <yungun200512@gmail.com>
2024-09-19 08:52:42 +0900

    전체 데이터를 읽은 후 처리하기

---
+++ src/main/java/chapter11/Code11_03.java
@@ -0,0 +1,24 @@
+package chapter11;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Code11_03 {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new FileReader("c:/FirstJava/myData1.txt"));
+        String inStr;
+
+        while(true) {
+            inStr = br.readLine();
+            if(inStr == null)
+                break;
+            sb.append(inStr).append("\n");
+        }
+
+        br.close();
+
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
