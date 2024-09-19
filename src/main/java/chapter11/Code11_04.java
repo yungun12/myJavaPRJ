9679d2cd26e8fd8f74f7c4a95f63094db333cd2f
yungun12 <yungun200512@gmail.com>
2024-09-20 08:46:31 +0900

    파일에 한행 쓰기

---
+++ src/main/java/chapter11/Code11_04.java
@@ -0,0 +1,22 @@
+package chapter11;
+
+import java.io.FileWriter;
+
+public class Code11_04 {
+    public static void main(String[] args) throws Exception {
+        FileWriter fw = new FileWriter("C:/FristJava.myData2.txt");
+        String outStr;
+
+        outStr = "르세라핌";
+        fw.write(outStr + "\n") ;
+
+        outStr = "아이브";
+        fw.write(outStr + "\n") ;
+
+        outStr = "뉴진스";
+        fw.write(outStr + "\n") ;
+
+        fw.close();
+        System.out.println("--- myData2.txt 파일에 저장됨 ---");
+    }
+}
